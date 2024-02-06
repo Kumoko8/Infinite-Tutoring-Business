cb3fae305e9549a633b74565236a180dedc03aee
Kumori <tauen8@gmail.com>
2024-02-05 21:33:52 -0600

    working on mapping the sections

--- infinite-tutoring/src/App.jsx
+++ infinite-tutoring/src/App.jsx
@@ -5,24 +5,45 @@ import Section from '../src/components/Section'
 import './App.css'
 
 function App() {
-
+const sections = [
+  {
+    title: "Infinite Tutoring",
+   
+},
+{
+  title: "Infinite Tutoring",
+  subtitle: "Expert Tutors to Help You Succeed",
+  text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...",
+  image: "/images/welcome-image.jpg",
+},
+]
   return (
     <>
-      <div>
-        <h1>Infinite Tutoring</h1>
 
-        <Element name="home">
-          <Parallax bgImage='../src/assets/logo.jpg' strength={500}>
-            <Section title="Infinite Tutoring" />
-          </Parallax>
+      <Parallax bgImage='../src/assets/logo.jpg' strength={1000}>
+        <Element name="home" class='card'>
+          <Section title={sections.title} />
+
         </Element>
         <Element name="about">
-          <Section title="About Us" />
-        </Element>
+          <Section subtitle="Who We Are" />
+<p> We help kids reach their greatest potential by first instilling confidence. We believe in the limitless potential of every student we serve.</p>
 
-      </div>
+        </Element>
+        <Element name="what">
+          <Section subtitle='What We Do'/>
+            <p> We offer a personal learning experience focused on using visuals, storytelling, and making connections. </p>
+          
+        </Element>
+      </Parallax>
     </>
   )
 }
 
 export default App
+
+//what is strength?
+//adjust logo
+//fill out sections and link buttons
+//minimal nav bar
+//sample content
--- infinite-tutoring/src/components/Section.jsx
+++ infinite-tutoring/src/components/Section.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-const Section = ({ title }) => {
+const Section = ({ title, subtitle, text, image}) => {
   return (
-    <div style={{ height: '500px', textAlign: 'center', paddingTop: '100px' }}>
-      <h2>{title}</h2>
+    <div style={{ width: '100%', height: '100%', textAlign: 'center'}}>
+      <h1>{title}</h1>
+      <h2>{subtitle} </h2>
+      <h3> {text} </h3>
+      <img src={image} alt={`Image for ${title}` } />
     </div>
   );
 };
